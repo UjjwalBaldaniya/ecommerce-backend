d30ffc68c05e7455dc5c81fb2b36b7bcb45c8d65
UjjwalBaldaniya <ujjwalbaldaniya@gmail.com>
2026-01-03 18:47:20 +0530

    chore: add husky and lint-staged for pre-commit checks

--- eslint.config.js
+++ eslint.config.js
@@ -21,7 +21,7 @@ export default [
       "prettier/prettier": "error",
 
       // TypeScript best practices
-      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/no-unused-vars": ["error"],
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/no-explicit-any": "warn",
 
