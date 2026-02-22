15e3d22c7b12df57e4ffdddea4c6c20de2a5fbea
UjjwalBaldaniya <ujjwalbaldaniya@gmail.com>
2026-02-22 22:27:53 +0530

    refactor(response): standardize API response structure

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,19 +4,29 @@ import {
   loginUser,
   registerUser,
 } from "../services/auth.service.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 export const register = async (req: Request, res: Response) => {
   const result = await registerUser(req.body);
-  return res.status(201).json({ success: true, data: result });
+
+  return res
+    .status(201)
+    .json(new ApiResponse("User registered successfully", result));
 };
 
 export const login = async (req: Request, res: Response) => {
   const result = await loginUser(req.body);
-  return res.status(200).json({ success: true, data: result });
+
+  return res
+    .status(200)
+    .json(new ApiResponse("User logged in successfully", result));
 };
 
 export const refreshToken = async (req: Request, res: Response) => {
   const { refreshToken } = req.body;
   const result = await handleRefreshToken(refreshToken);
-  res.json({ success: true, data: result });
+
+  return res
+    .status(200)
+    .json(new ApiResponse("Access token refreshed", result));
 };
